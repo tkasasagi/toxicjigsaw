91bff1b4a74a79546c9822436200e4d0f37b6ef3
Tarin Clanuwat <t.kasasagi@gmail.com>
2018-03-05 18:09:25 +0900

    Still can't do anything

--- kernel3.py
+++ kernel3.py
@@ -7,15 +7,19 @@ import xgboost as xgb
 
 class_names = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 
+# Load the dataset
 df_train = pd.read_csv('input/train.csv')
 df_test = pd.read_csv('input/test.csv')
 
 print(df_train.head(), '\n' ,df_test.head())
 
+
+#Add comment_text in train and test into a big chunk
 train_text = df_train['comment_text']
 test_text  = df_test['comment_text']
 all_text = pd.concat([train_text, test_text])
 
+#Change the whole text words into vector (Word Level)
 word_vectorizer = TfidfVectorizer(
     sublinear_tf=True,
     strip_accents='unicode',
@@ -23,31 +27,76 @@ word_vectorizer = TfidfVectorizer(
     token_pattern=r'\w{1,}',
     stop_words='english',
     ngram_range=(1, 1),
-    max_features=20000)
+    max_features=60000)
     #original max_features=10000.
 word_vectorizer.fit(all_text)
+
+#Change the train and test text into vector (Word Level)
 train_word_features = word_vectorizer.transform(train_text)
 test_word_features = word_vectorizer.transform(test_text)
 
-char_vectorizer = TfidfVectorizer(
-    sublinear_tf=True,
-    strip_accents='unicode',
-    analyzer='char',
-    stop_words='english',
-    ngram_range=(2, 6),
-    max_features=50000)
-char_vectorizer.fit(all_text)
-train_char_features = char_vectorizer.transform(train_text)
-test_char_features = char_vectorizer.transform(test_text)
+print(train_word_features.shape[1])
+print(test_word_features.shape)
 
-train_features = hstack([train_char_features, train_word_features])
-test_features = hstack([test_char_features, test_word_features])
+total = train_word_features.shape[1] + 1
+
+train = pd.DataFrame()
+
+len(train_word_features)
+
+A = np.zeros(60000)
+
+for i in range(0, train_word_features.shape[0]):
+    a = []
+    for j in range(0, train_word_features.shape[1] ):
+        a = np.append(a, train_word_features[i, j])
+    A = np.vstack([A, a])
+print(a)
+
+y_train = df_train.iloc[:, 2:8]
+print(y_train.shape)
+y1 = y_train['toxic']
+y2 = y_train['severe_toxic']
+y3 = y_train['obscene']
+y4 = y_train['threat']
+y5 = y_train['insult']
+y6 = y_train['identity_hate']
+
+#######################  XGBOOST  #######################
+# Set xgboost parameters
+params = {}
+params['objective'] = 'binary:logistic'
+params['eta'] = 0.02
+params['silent'] = True
+params['max_depth'] = 4
+#params['subsample'] = 0.9
+#params['colsample_bytree'] = 0.9
+
+#----1
+d_train = xgb.DMatrix(train_features, y1)
+d_test = xgb.DMatrix(test_features)
+
+print(d_train)
+
+mdl = xgb.train(params, d_train, 10000, early_stopping_rounds=100, maximize=True, verbose_eval=10)
+p_test1 = mdl.predict(d_test)
+
+#----2
+d_train = xgb.DMatrix(train_features, y2)
+
+mdl = xgb.train(params, d_train, 10000, early_stopping_rounds=100, maximize=True, verbose_eval=10)
+p_test1 = mdl.predict(d_test)
+#----3
+
+#----4
+
+#----5
+
+#----6
 
 
 
-
-
-
+#------------------ SAVE FILE ---------------------------
 
 """
 
